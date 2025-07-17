dcc2a206929dd204238b9aa9baf6f46c49b3b247
tlext123 <150935019+tlext123@users.noreply.github.com>
2025-07-17 16:05:47 +0100

    Added dynamic chunking for pdfs of different sizes

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -12,6 +12,23 @@ def clean_text(text):
     text = re.sub(r"\n", " ", text)
     return text.strip()
 
+def get_dynamic_chunk_params(text: str):
+
+    length = len(text)
+    if length < 2000:
+        return 100, 10
+    elif length < 10000:
+        return 300, 30
+    else:
+        return 600, 50
+
+def split_text_dynamically(text: str):
+    chunk_size, overlap = get_dynamic_chunk_params(text)
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size = chunk_size,
+        overlap = overlap
+    )
+    return splitter.split_text(text)
 
 def answer_question_from_pdf(doc_path, question):
 
@@ -23,8 +40,7 @@ def answer_question_from_pdf(doc_path, question):
 
     text = clean_text(text)
 
-    splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=20)
-    chunks = splitter.split_text(text)
+    chunks = split_text_dynamically(text)
 
     embeddings = HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
     db = FAISS.from_texts(chunks, embedding=embeddings)
@@ -49,9 +65,9 @@ def answer_question_from_pdf(doc_path, question):
     return answer
 
 
-if __name__ == '__main__':
-    answer = answer_question_from_pdf(r"C:\Users\tlext\Desktop\MASTERS\DISSERTATION\2498906.pdf", "What is this document about?")
-    print('Answer:', answer)
+# if __name__ == '__main__':
+#     answer = answer_question_from_pdf(r"C:\Users\tlext\Desktop\MASTERS\DISSERTATION\2498906.pdf", "What is this document about?")
+#     print('Answer:', answer)
 
 
 
